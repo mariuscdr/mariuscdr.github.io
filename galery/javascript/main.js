507238b19183ae13310bf0bc8f55bb5b5092463d
mariuscdr <44243943+mariuscdr@users.noreply.github.com>
2020-09-27 15:58:07 +0200

    Update main.js

--- galery/javascript/main.js
+++ galery/javascript/main.js
@@ -2,6 +2,8 @@
 
 $( document ).ready(function() {
 
+
+
   $('#anim-titre').css(
     {
      'margin-top' : +($(window).height()*0.5) + "px"
@@ -9,14 +11,14 @@ $( document ).ready(function() {
 
      $('#anim-paysage').css(
        {
-        'margin-top' : +($(window).height()*1.5) + $(this).scrollTop()/3 + "px"
+        'margin-top' : +($(window).height()*1.5) + "px"
         });
 $(window).scroll(function () {
     if ($(this).scrollTop() < $(window).height()/1.2)
       {
          $('#anim-titre').css(
            {
-            'margin-top' : +($(window).height()*0.5) + $(this).scrollTop()/3 + "px"
+            'margin-top' : +($(window).height()*0.5) + $(this).scrollTop()*0.2 + "px"
             });
        }
 
@@ -32,4 +34,36 @@ $(window).scroll(function () {
          }
 
         });
+
+    $('#fl-right').click(function() {
+      $('#defilement').css(
+        {
+
+        }
+      )
+
+    });
+
+    var clic = 0;
+    $('#defilement').click(function() {
+      if (clic == 0)
+      {
+        $('.defilement').css(
+          {
+            'width' : 55 + "vw"
+          })
+          clic = 1;
+      }
+      else if (clic == 1)
+      {
+        $('.defilement').css(
+          {
+            'width' : 40 + "vw"
+          })
+          clic = 0;
+      }
+
+    });
+
+
 });
